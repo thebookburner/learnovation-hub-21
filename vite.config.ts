d422279b2cfc9defa101c1c50875010fb7278857
curiosityz <148975934+curiosityz@users.noreply.github.com>
2024-12-14 17:01:34 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/learnovation-hub-21/',  // 
   server: {
     host: "::",
     port: 8080,
